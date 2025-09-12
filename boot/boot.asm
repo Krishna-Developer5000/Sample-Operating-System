498cf7b0d44c3b763d85b950ab2fc976fd0b972d
Krishna-Developer5000 <kkuppuluri0@gmail.com>
2025-09-12 20:51:05 +0530

    Changes undone to boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -7,7 +7,6 @@ start:
     mov al, 'K'     ; Print the letter 'K'
     int 0x10        ; Call BIOS interrupt to print
 
-    jmp $           ; Infinite loop to freeze
-
+    jmp $
 times 510 - ($ - $$) db 0  ; Fill to 512 bytes
 dw 0xAA55                 ; Boot signature (required)
